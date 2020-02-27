9cd5b92d3a8c46ebb2ffc56f9ca0163b7de8066b
Danye <danielbrzen@outlook.com>
2020-02-27 02:13:50 -0600

    INTERFAZ CON METODOS ABSTRACTOS QUE REALIZARA MONITOR

---
+++ src/comegalleta/Interfaz/interfazComegalletas.java
@@ -0,0 +1,11 @@
+package comegalleta.Interfaz;
+
+public interface interfazComegalletas {
+
+    public void consumirGalletas();
+
+    public void prepararGalletas(short galletasProducidas);
+
+    public void VerificarEstatusFrasco();
+
+}
