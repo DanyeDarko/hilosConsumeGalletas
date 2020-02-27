d090217d7646d1483da94149af7cb1a6022f705b
Danye <danielbrzen@outlook.com>
2020-02-27 02:14:49 -0600

    HILO PRODUCTOR DE GALLETAS COMPARTE UN MONITOR 'frascoGalletas' Y SUS DATOS

--- src/comegalleta/ObjetosComegalletas/MamaHilo.java
+++ src/comegalleta/ObjetosComegalletas/MamaHilo.java
@@ -1,40 +1,35 @@
-
 package comegalleta.ObjetosComegalletas;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class MamaHilo  extends Thread {
-
+public class MamaHilo extends Thread {
+    
+    //<editor-fold defaultstate="collapsed" desc="VARIABLES GLOBALES HILO">
     frascoDeGalletas frascoConGalletas;
     short galletasProducir;
     int numeroHilo;
+   //</editor-fold>
     
-    public MamaHilo(frascoDeGalletas nuevoFrasco,short galletas,int numHilo) {
-        
+    
+    public MamaHilo(frascoDeGalletas nuevoFrasco, short galletas, int numHilo) {
+
         this.frascoConGalletas = nuevoFrasco;
         this.galletasProducir = galletas;
         this.numeroHilo = numHilo;
     }
 
-public void run(){
+    public void run() {
         try {
-            System.out.println("");
-            System.out.println("\t\t°°° MAMA numero " +numeroHilo+ " COCINANDO GALLETAS ");
-            frascoConGalletas.prepararGalletas(galletasProducir);
+           
+            System.out.println("\n\t\t°°° MAMA numero " + numeroHilo + " COCINANDO GALLETAS ");
+            frascoConGalletas.prepararGalletas(galletasProducir); 
             sleep(8000);
-            /*
-            System.out.println("°°° AL MOMENTO DE ESTE HILO HAY "+frascoConGalletas.numGalletas);
-            
-            
-            System.out.println("°°° MIENTRAS MAMA COCINA 5 GALLETAS EL FRASCO ESTA "+frascoConGalletas.frascoLleno);
-            frascoConGalletas.prepararGalletas(galletasProducir);
-            */
+
         } catch (InterruptedException ex) {
             System.out.println("ocurrio un error durmiendo al hilo MAMA");
         }
-        
-}
 
-    
+    }
+
 }
