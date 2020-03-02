6be41d9d9de504b87316baea92314ff717add263
daniel <danielbrzen@outlook.com>
2020-03-02 11:54:38 -0600

    Modifi constructor y parametros  quite sleep()

--- src/comegalleta/ObjetosComegalletas/niñoHilo.java
+++ src/comegalleta/ObjetosComegalletas/niñoHilo.java
@@ -5,11 +5,9 @@ package comegalleta.ObjetosComegalletas;
 public class niñoHilo extends Thread {
 
     frascoDeGalletas frascoConGalletas;
-    short cantidadGalletasPermitidas;
     int numeroHilo;
-    public niñoHilo(frascoDeGalletas nuevoFrasco,short galletasPermitidas,int numHilo) {
+    public niñoHilo(frascoDeGalletas nuevoFrasco,int numHilo) {
         this.frascoConGalletas = nuevoFrasco;
-        this.cantidadGalletasPermitidas = galletasPermitidas;
         this.numeroHilo = numHilo;
     }
 
@@ -17,10 +15,10 @@ public class niñoHilo extends Thread {
     
     public void run(){
        
-        try {
-             System.out.println("\t------- SE LANZA HILO HIJO "+numeroHilo +" HAY " +frascoConGalletas.numGalletas + "GALLETAS " );  
+       
+        System.out.println("\t------- SE LANZA HILO HIJO "+numeroHilo +" HAY " +frascoConGalletas.pila.size() + " GALLETAS " );  
         frascoConGalletas.consumirGalletas();
-            sleep(50000);
+           
 //System.out.println("\t***SE LANSA HILO HIJO::: "+numeroHilo+" \n\t HAY " +frascoConGalletas.numGalletas + " GALLETAS EN EL FRASCO");
 /*
 System.out.println("\t***EL FRASCO ESTA LLENO (TRUE) ? ::: "+frascoConGalletas.frascoLleno+"");
@@ -28,9 +26,8 @@ System.out.println("\t***EL FRASCO ESTA DISPONIBLE (TRUE) ? ::: "+frascoConGalle
 frascoConGalletas.consumirGalletas();
 System.out.println("\t***HILO HIJO "+numeroHilo+" CONSUMI 3 GALLETAS :::");
 */
-        } catch (InterruptedException ex) {
-            System.out.println("ocurrio un error durmiendo al hilo HIJO");
-        }
-    }
+      
     
+    
+}
 }
