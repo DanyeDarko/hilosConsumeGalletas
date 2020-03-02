37ca79188e7ad033321a754c41886770e79b0e4c
daniel <danielbrzen@outlook.com>
2020-03-02 11:53:54 -0600

    Mod constructor y quitamos sleep

--- src/comegalleta/ObjetosComegalletas/MamaHilo.java
+++ src/comegalleta/ObjetosComegalletas/MamaHilo.java
@@ -6,29 +6,23 @@ public class MamaHilo extends Thread {
     
     //<editor-fold defaultstate="collapsed" desc="VARIABLES GLOBALES HILO">
     frascoDeGalletas frascoConGalletas;
-    short galletasProducir;
     int numeroHilo;
    //</editor-fold>
     
     
-    public MamaHilo(frascoDeGalletas nuevoFrasco, short galletas, int numHilo) {
-
+    public MamaHilo(frascoDeGalletas nuevoFrasco, int numHilo) {
         this.frascoConGalletas = nuevoFrasco;
-        this.galletasProducir = galletas;
         this.numeroHilo = numHilo;
     }
 
     public void run() {
-        try {
-           
+        
             System.out.println("\n\t\t°°° MAMA numero " + numeroHilo + " COCINANDO GALLETAS ");
-            frascoConGalletas.prepararGalletas(galletasProducir); 
-            sleep(8000);
+            frascoConGalletas.prepararGalletas(); 
 
-        } catch (InterruptedException ex) {
-            System.out.println("ocurrio un error durmiendo al hilo MAMA");
-        }
+        
 
-    }
+   
 
+}
 }
